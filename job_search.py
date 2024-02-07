d00d58475b0cdc38a04736dc27a74676b12beaae
Lucas Bispo <lucas_bispo@hotmail.com>
2024-02-06 23:04:31 -0300

    teste job search

---
+++ job_search.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+class job_search(scrap):
+    
